e9ad4dfceea47663acd73dfc10729d30eaf100f5
Katsutami7moto <nclsf87@gmail.com>
2016-10-14 01:27:41 +0300

    Script is working

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,17 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5667329
 
 
 def get_user_login():
-    pass
+    login = input('Enter your login: ')
+    return login
 
 
 def get_user_password():
-    pass
+    password = getpass.getpass(prompt='Enter your password: ')
+    return password
 
 
 def get_online_friends(login, password):
@@ -17,16 +19,26 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_online_ids = api.friends.getOnline()
+    return api.users.get(user_ids=friends_online_ids)
 
 
-def output_friends_to_console(friends_online):
-    pass
+def output_friends_to_console(online_friends):
+    print('\nFriends online:\n')
+    for friend in online_friends:
+        print(friend['first_name'], friend['last_name'])
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    current_login = get_user_login()
+    current_password = get_user_password()
+    try:
+        friends_online = get_online_friends(current_login, current_password)
+    except vk.exceptions.VkAuthError as error_message:
+        print(error_message)
+        exit(1)
+    else:
+        output_friends_to_console(friends_online)
+        input()
